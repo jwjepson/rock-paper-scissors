89e85f0a1e01d44107f5d4b3fa70ea11b6f2bdc8
Jake Jepson <jwjepson1@gmail.com>
2022-08-12 00:18:50 -0400

    create simulateRound function

--- myscripts.js
+++ myscripts.js
@@ -3,3 +3,28 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
+
+function simulateRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        return "tie";
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "lose";
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "win";
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "win";
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "lose";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "lose";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "win";
+    }
+}
